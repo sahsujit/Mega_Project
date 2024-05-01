03db3a3c288ccf6c4cceb8328d2b910f6f61cddc
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-05-01 12:23:06 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/common/Navbar";
 import Catalog from "./pages/Catalog";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import OpenRoute from "./components/core/Auth/OpenRoute"
+import ForgotPassword from "./pages/ForgotPassword";
+import UpdataPassword from "./pages/UpdataPassword";
 
 
 function App() {
@@ -13,8 +15,8 @@ function App() {
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="catalog/:catalogName" element={<Catalog/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="catalog/:catalogName" element={<Catalog />} />
 
         <Route
           path="signup"
@@ -24,11 +26,28 @@ function App() {
             </OpenRoute>
           }
         />
-    <Route
+        <Route
           path="login"
           element={
             <OpenRoute>
               <Login />
+            </OpenRoute>
+          }
+        />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdataPassword />
             </OpenRoute>
           }
         />
