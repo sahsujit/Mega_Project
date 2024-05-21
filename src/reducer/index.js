6bd67d8a05eb73bcad3b6bf06592194413b6a9bb
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-05-21 12:50:47 +0545

    Update index.js

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -3,6 +3,8 @@ import authReducer from "../slices/authSlice"
 import profileReducer from "../slices/profileSlice"
 import cartReducer from "../slices/cartSlice";
 import courseReducer from "../slices/courseSlice"
+import viewCourseReducer from "../slices/viewCourseSlice"
+
 
 
 const rootReducer = combineReducers({
@@ -10,6 +12,8 @@ const rootReducer = combineReducers({
     profile:profileReducer,
     cart:cartReducer,
     course:courseReducer,
+    viewCourse:viewCourseReducer,
+
 });
 
 export default rootReducer;
