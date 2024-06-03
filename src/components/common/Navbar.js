4fa9364691449a8b0849b49cfacc0cdbdf1010ab
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-06-03 20:36:42 +0545

    Update Navbar.js

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,60 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import { Link, matchPath } from 'react-router-dom'
+
+import ProfileDropdown from '../core/Auth/ProfileDropdown'
+
+import { useEffect, useState } from "react"
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
+import { BsChevronDown } from "react-icons/bs"
+import { useSelector } from "react-redux"
+import { Link, matchPath, useLocation } from "react-router-dom"
+
 import logo from "../../assets/Logo/Logo-Full-Light.png"
 import { NavbarLinks } from "../../data/navbar-links"
-import { useLocation } from 'react-router-dom'
-import { BsChevronDown } from "react-icons/bs"
+import { apiConnector } from "../../services/apiconnectors"
+import { categories } from "../../services/api"
+import { ACCOUNT_TYPE } from "../../utils/constants"
+// import ProfileDropdown from "../core/Auth/ProfileDropDown"
 
-import { useSelector } from 'react-redux'
-import { AiOutlineShoppingCart } from "react-icons/ai"
-import { ACCOUNT_TYPE } from '../../utils/constants'
-import ProfileDropdown from '../core/Auth/ProfileDropdown'
-import { apiConnector } from '../../services/apiconnectors'
-import { categories } from '../../services/api'
+function Navbar() {
+  const { token } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.profile)
+  const { totalItems } = useSelector((state) => state.cart)
+  const location = useLocation()
 
+  const [subLinks, setSubLinks] = useState([])
+  const [loading, setLoading] = useState(false)
 
-const Navbar = () => {
+  useEffect(() => {
+    ;(async () => {
+      setLoading(true)
+      try {
+        const res = await apiConnector("GET", categories.CATEGORIES_API)
+        setSubLinks(res.data.data)
+      } catch (error) {
+        console.log("Could not fetch Categories.", error)
+      }
+      setLoading(false)
+    })()
+  }, [])
 
-    const { user } = useSelector((state) => state.profile);
-    const { token } = useSelector((state) => state.auth);
-    const { totalItems } = useSelector((state) => state.cart)
+  // console.log("sub links", subLinks)
 
-    const [subLinks, setSubLinks] = useState([]);
-    const [loading, setLoading] = useState(false)
+  const matchRoute = (route) => {
+    return matchPath({ path: route }, location.pathname)
+  }
 
-
-    useEffect(() => {
-        ;(async () => {
-          setLoading(true)
-          try {
-            const res = await apiConnector("GET", categories.CATEGORIES_API)
-            setSubLinks(res.data.allCategorys)
-           
-
-          } catch (error) {
-            console.log("Could not fetch Categories.", error)
-          }
-          setLoading(false)
-        })()
-      }, [])
-    
-
-    const location = useLocation();
-    const matchRoute = (route) => {
-        return matchPath({ path: route }, location.pathname)
-    }
-
-    return (
-        <div className='flex items-center justify-center h-14 border-b-[1px] border-richblack-700'>
-            <div className="flex w-11/12 max-w-maxContent items-center justify-between">
-
-                {/* image */}
-                <Link to="/">
-                    <img src={logo} alt="Log" width={160} height={32} loading='lazy' />
-                </Link>
-
-                {/* nav link */}
-                <nav className="hidden md:block">
+  return (
+    <div
+      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
+        location.pathname !== "/" ? "bg-richblack-800" : ""
+      } transition-all duration-200`}
+    >
+      <div className="flex w-11/12 max-w-maxContent items-center justify-between">
+        {/* Logo */}
+        <Link to="/">
+          <img src={logo} alt="Logo" width={160} height={32} loading="lazy" />
+        </Link>
+        {/* Navigation links */}
+        <nav className="hidden md:block">
           <ul className="flex gap-x-6 text-richblack-25">
             {NavbarLinks.map((link, index) => (
               <li key={index}>
@@ -115,42 +115,40 @@ const Navbar = () => {
             ))}
           </ul>
         </nav>
-
-                {/* login/signup/dashboard */}
-                <div className='md:flex  hidden gap-x-8 items-center'>
-
-                    {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
-                        <Link to="/dashboard/cart" className="relative">
-                            <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
-                            {totalItems > 0 && (
-                                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
-                                    {totalItems}
-                                </span>
-                            )}
-                        </Link>
-                    )}
-
-                    {token === null && (
-                        <Link to="/login">
-                            <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
-                                Log in
-                            </button>
-                        </Link>
-                    )}
-                    {token === null && (
-                        <Link to="/signup">
-                            <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
-                                Sign up
-                            </button>
-                        </Link>
-                    )}
-                    {token !== null && <ProfileDropdown />}
-
-                </div>
-            </div>
-
+        {/* Login / Signup / Dashboard */}
+        <div className="hidden items-center gap-x-4 md:flex">
+          {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
+            <Link to="/dashboard/cart" className="relative">
+              <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
+              {totalItems > 0 && (
+                <span className="absolute -bottom-2 -right-2 grid h-5 w-5 place-items-center overflow-hidden rounded-full bg-richblack-600 text-center text-xs font-bold text-yellow-100">
+                  {totalItems}
+                </span>
+              )}
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/login">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Log in
+              </button>
+            </Link>
+          )}
+          {token === null && (
+            <Link to="/signup">
+              <button className="rounded-[8px] border border-richblack-700 bg-richblack-800 px-[12px] py-[8px] text-richblack-100">
+                Sign up
+              </button>
+            </Link>
+          )}
+          {token !== null && <ProfileDropdown />}
         </div>
-    )
+        <button className="mr-4 md:hidden">
+          <AiOutlineMenu fontSize={24} fill="#AFB2BF" />
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
