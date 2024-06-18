853d9459113a39da22d9efff76444e9ed099a8f0
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-06-18 11:09:57 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import {configureStore} from "@reduxjs/toolkit"
-import rootReducer from "./reducer"
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
 import { Toaster } from "react-hot-toast";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+
+// Initialize Stripe with your publishable key
+// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY);
 
 const store = configureStore({
-  reducer:rootReducer,
+  reducer: rootReducer,
 });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-        <Toaster/>
-      </BrowserRouter>
-    </Provider>
-
-
-  </React.StrictMode>
+  <Provider store={store}>
+  <BrowserRouter>
+      <App/>
+   
+      <Toaster/>
+    </BrowserRouter>
+  </Provider>
 );
+
+export { store };
