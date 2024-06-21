fba8410cd209ec5471bfade6fbfaae4860275405
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-06-21 21:48:14 +0545

    Update cartSlice.js

--- src/slices/cartSlice.js
+++ src/slices/cartSlice.js
@@ -1,38 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { toast } from "react-hot-toast"
-
-// const initialState = {
-    
-//     totalItems : localStorage.getItem("totalItems") ? 
-//     JSON.parse(localStorage.getItem("totalItems")) : 0
-// }
-
-// const cartSlice = createSlice({
-//     name:"cart",
-//     initialState : initialState,
-//     reducers:{
-//         setTotalItems(state , value) {
-//             state.totalItems = value.payload
-//         }
-//     }
-// });
-
-// export const {setTotalItems} = cartSlice.actions;
-// export default  cartSlice.reducer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import { createSlice } from "@reduxjs/toolkit"
 import { toast } from "react-hot-toast"
