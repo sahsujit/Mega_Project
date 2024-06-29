076451d555a669156aa66b2234c5f7e934259ebb
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-06-29 12:13:41 +0545

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,6 +9,7 @@ import StatsComponenet from '../components/core/AboutPage/StatsComponent'
 import LearningGrid from '../components/core/AboutPage/LearningGrid'
 import ContactFormSection from '../components/core/AboutPage/ContactFormSection'
 import Footer from "../components/common/Footer"
+import ReviewSlider from '../components/common/ReviewSlider'
 
 
 const About = () => {
@@ -122,8 +123,9 @@ const About = () => {
         <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
         </h1>
-        {/* <ReviewSlider /> */}
-        {/* <ReviewSlider /> */}
+       
+        <ReviewSlider/>
+     
       </div>
 
 
